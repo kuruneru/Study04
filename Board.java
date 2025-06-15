17bf9ebb6ac9304778d0fc4d5ad0503be4ae51b9
kuruneru <kuruneru.ikkou@gmail.com>
2025-06-16 08:05:28 +0900

    完成

--- Board.java
+++ Board.java
@@ -1,31 +1,105 @@
 public class Board {
     public int[][] square;
     public int turn;
+    java.util.Random rand = new java.util.Random(0); //乱数は実行毎に同じ結果を得るため, 乱数のseed を0 に設定する
     
     Board() {
 	square = new int[5][5];
-	for (int i=0; i<5; i++) {
-	    for (int j=0; j<5; j++) {
-		square[i][j] = 0;
-	    }
-	}
-	turn = 1;
+        for (int i=0; i<5; i++) {
+            for (int j=0; j<5; j++) {
+                square[i][j] = 0;
+            }
+        }
+        turn = 1;
     }
 
     // x,y に置くことができるか
     public boolean isLegal(int x, int y) {
-	return false;
+        /* 置く場所があるまで値を変え続ける */
+        do {
+            x = rand.nextInt(5);
+            y = rand.nextInt(5);
+        } while (this.square[y][x] != 0);
+
+        /* もし無いのならfalseを返す */
+        if (this.isEnd()) {
+            return isEnd();
+        }
+        
+        /* 置ける場所があるのでその場所にそのターンのときのプレイヤーがコマを置く */
+        if (this.turn == 1) {
+            this.square[y][x] = 1;
+            System.out.println("先手は " + x + " " + y + " に置きました");
+            this.turn = 2;
+            return true;
+        } else {
+            this.square[y][x] = 2;
+            this.turn = 1;
+            System.out.println("後手は " + x + " " + y + " に置きました");
+            return true;
+        }
     }
     // ゲームの終了判定 : 置ける場所がない（勝敗がついた時とは別）
     public boolean isEnd() {
-	return false;
+
+        for (int nx=0; nx<5; nx++) {
+            for (int ny=0; ny<5; ny++) {
+                /* 置ける場所があるのならfalseを返す */
+                if (this.square[ny][nx] == 0) {
+                    return false;
+                }
+            }
+        }
+
+        /* すべて探索した後，置ける場所が無いのでtrueが返る */
+	    return true;
     }
     // 最後に着手したプレイヤが勝った（4目並べた）か判定
     public boolean isWinning() {
-	return false;
+
+        for (int ny=0; ny<5; ny++) {
+            int numx = 0;
+            for (int nx=0; nx<5; nx++) {
+                if (this.square[ny][nx] == 1) {
+                    numx++;
+                    /* 4つ並んでいる状態なのでtrueが返る */
+                    if (numx == 4) {
+                        return true;
+                    }
+                } else {
+                    numx = 0;
+                }
+            }
+        }
+
+        for (int nx=0; nx<5; nx++) {
+            int numy = 0;
+            for (int ny=0; ny<5; ny++) {
+                if (this.square[ny][nx] == 1) {
+                    numy++;
+                    if (numy == 4) {
+                        return true;
+                    }
+                } else numy = 0;
+            }
+        }
+
+        /* まだ終わっていないのでfalseを返す */
+	    return false;
     }
     
     // 盤面表示
     public void showBoard() {
+        for (int i=0; i<5; i++) {
+            for (int j=0; j<5; j++) {
+                switch(this.square[i][j]) {
+                case 0: System.out.print("_"); break;
+                case 1: System.out.print("o"); break;
+                case 2: System.out.print("x"); break;
+                }
+            }
+
+            System.out.println();
+		}
     }
 }
--- C4.java
+++ C4.java
@@ -1,158 +1,32 @@
 public class C4 {
     static public void main(String[] args) {
-	java.util.Scanner scan = new java.util.Scanner(System.in);
-	java.util.Random rand = new java.util.Random(0); //乱数は実行毎に同じ結果を得るため, 乱数のseed を0 に設定する
-	Board b = new Board();
-	
-	boolean gameEnd = false; // ゲームが終わったか
-	boolean hasEmpty = true; // 空きマスがあるか
-	while (true) {
-	    int x = -1, y = -1;
-	    // 先手番（人間）
-	    for (int i=0; i<5; i++) {
-		for (int j=0; j<5; j++) {
-		    switch(b.square[i][j]) {
-		    case 0: System.out.print("_"); break;
-		    case 1: System.out.print("o"); break;
-		    case 2: System.out.print("x"); break;
-		    }
-		}
-		System.out.println();
-	    }
-	    do {
-	    	System.out.print("配置(x, y) を入力してください x(0-4) y(0-4):");
-	    	x = scan.nextInt();
-	    	y = scan.nextInt();
-	    } while (b.square[y][x] != 0);
-	    b.square[y][x] = 1;
-	    b.turn = 2;
-	    System.out.println("先手は " + x + " " + y + " に置きました");
-	    
-	    // 勝敗判定
-	    for (int ny=0; ny<5; ny++) {
-		if (gameEnd) break;
-		int numx = 0;
-		for (int nx=0; nx<5; nx++) {
-		    if (b.square[ny][nx] == 1) {
-			numx++;
-			if (numx == 4) {
-			    gameEnd = true;
-			    break;
-			}
-		    } else {
-			numx = 0;
-		    }
-		}
-	    }
-	    for (int nx=0; nx<5; nx++) {
-		if (gameEnd) break;
-		int numy = 0;
-		for (int ny=0; ny<5; ny++) {
-		    if (b.square[ny][nx] == 1) {
-			numy++;
-			if (numy == 4) {
-			    gameEnd = true;
-			    break;
-			}
-		    } else {
-			numy = 0;
-		    }
-		}
-	    }
-	    if (gameEnd) break;
-	    hasEmpty = false;
-	    for (int nx=0;nx<5;nx++) {
-		if (hasEmpty) break;
-		for (int ny=0;ny<5;ny++) {
-		    if (b.square[ny][nx] == 0) {
-			hasEmpty = true;
-			break;
-		    }
-		}
-	    }
-	    if (!hasEmpty) break;
+		Board b = new Board();
 
-	    // 後手番（計算機: ランダム）
-	    for (int i=0; i<5; i++) {
-		for (int j=0; j<5; j++) {
-		    switch(b.square[i][j]) {
-		    case 0: System.out.print("_"); break;
-		    case 1: System.out.print("o"); break;
-		    case 2: System.out.print("x"); break;
-		    }
-		}
-		System.out.println();
-	    }
+		/* ターンが1であれば先手判定，ターンが2であれば後手判定 */
+		while (true) {
+			int x = -1, y = -1;
+			// 先手番（人間）
+			b.showBoard();
 
-	    do {
-	    	x = rand.nextInt(5);
-	    	y = rand.nextInt(5);
-	    } while (b.square[y][x] != 0);
-	    b.square[y][x] = 2;
-	    b.turn = 1;
-	    System.out.println("後手は " + x + " " + y + " に置きました");
+			/* おけるかどうか */
+			b.isLegal(x, y);
+			
+			// 勝敗判定
+			if (b.isWinning()) break;
+			// 空白判定
+			if (b.isEnd()) break;
 
-	    for (int ny=0; ny<5; ny++) {
-		if (gameEnd) break;
-		int numx = 0;
-		for (int nx=0; nx<5; nx++) {
-		    if (b.square[ny][nx] == 2) {
-			numx++;
-			if (numx == 4) {
-			    gameEnd = true;
-			    break;
-			}
-		    } else {
-			numx = 0;
-		    }
 		}
-	    }
-	    for (int nx=0; nx<5; nx++) {
-		if (gameEnd) break;
-		int numy = 0;
-		for (int ny=0; ny<5; ny++) {
-		    if (b.square[ny][nx] == 2) {
-			numy++;
-			if (numy == 4) {
-			    gameEnd = true;
-			    break;
-			}
-		    } else {
-			numy = 0;
-		    }
-		}
-	    }
-	    if (gameEnd) break;
-	    hasEmpty = false;
-	    for (int nx=0;nx<5;nx++) {
-		if (hasEmpty) break;
-		for (int ny=0;ny<5;ny++) {
-		    if (b.square[ny][nx] == 0) {
-			hasEmpty = true;
-			break;
-		    }
-		}
-	    }
-	    if (!hasEmpty) break;
 
+		b.showBoard();
+
+		/* 最終結果発表 */
+		if (b.isEnd()) {
+			System.out.println("引き分けです");
+		} else if (b.turn == 1) {
+			System.out.println("後手が勝ちました");
+		} else {
+			System.out.println("先手が勝ちました");
+		}
 	}
-	for (int i=0; i<5; i++) {
-	    for (int j=0; j<5; j++) {
-		switch(b.square[i][j]) {
-		case 0: System.out.print("_"); break;
-		case 1: System.out.print("o"); break;
-		case 2: System.out.print("x"); break;
-		}
-	    }
-	    System.out.println();
-	}
-
-	if (!hasEmpty) {
-	    System.out.println("引き分けです");
-	} else if (b.turn == 1) {
-	    System.out.println("後手が勝ちました");
-	} else {
-	    System.out.println("先手が勝ちました");
-	}
-    }
 }
